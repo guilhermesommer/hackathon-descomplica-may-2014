0b3990fc5eea8f32e5dab0eb021550247ddffdc0
guilhermesommer <guilhermesommer@gmail.com>
2014-05-28 16:31:53 -0300

    criação da controller e da view

---
+++ Source/Hackaton/Controllers/TvController.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LiveController.cs" company="Descomplica">
+//   Descomplica
+// </copyright>
+// <summary>
+//   Defines the LiveController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Hackaton.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The television controller.
+    /// </summary>
+    public class TvController : Controller
+    {
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+    }
+}
